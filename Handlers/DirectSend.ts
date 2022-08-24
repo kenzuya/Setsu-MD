1d96f9adf7a565ce1f6e2e132a4407a478edd58c
Setsu <pbgarena0838@gmail.com>
2022-08-24 17:33:46 +0700

    Add private handler: Direct send from url

---
+++ Handlers/DirectSend.ts
@@ -0,0 +1,15 @@
+import { WAMethods } from "../Library/Functions";
+import { ucapanWaktu, useSessionUUID } from "../Library/Modules";
+import { MessageSerializer } from "../Library/Serialize";
+import * as config from "../Config/Config.json";
+import { custom_msg } from "../Library/Fakes";
+const handler = (setsu: WAMethods, m: MessageSerializer) => {
+    useSessionUUID(m.args![0], async (result) => {
+        if (result) {
+            return setsu.sendMedia(m.chat, result, config.mess.done, custom_msg(ucapanWaktu()! + m.pushname));
+        } else return m.reply(`${m.bold}Session telah expired...${m.bold}\nKirim ulang perintahnya!`);
+    });
+};
+handler.command = ["__directsendfromurl"];
+
+export default handler;
