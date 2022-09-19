d7c187aaccb18d89560e2b3a780890d2a207f0f8
Kenzuya <pbgarena0838@gmail.com>
2022-09-19 21:15:06 +0700

    feat: add wait message

--- Handlers/DirectSend.ts
+++ Handlers/DirectSend.ts
@@ -6,6 +6,7 @@ import { custom_msg } from "../Library/Fakes";
 const handler = (setsu: WAMethods, m: MessageSerializer) => {
     useSessionUUID(m.args![0], async (result) => {
         if (result) {
+            m.reply(config.mess.wait);
             return setsu.sendMedia(m.chat, result, config.mess.done, custom_msg(ucapanWaktu()! + m.pushname));
         } else return m.reply(`${m.bold}Session telah expired...${m.bold}\nKirim ulang perintahnya!`);
     });
