972bed2116d668cfb2a47a6c0e4507622c06d9c5
Kenzuya <pbgarena0838@gmail.com>
2022-09-25 09:25:28 +0700

    chore: updated install script

--- Library/_install-ubuntu.sh
+++ Library/_install-ubuntu.sh
@@ -1,28 +1,8 @@
 echo "Updating system..."
 sudo apt update
 sudo apt upgrade -y
-echo "Completed..."
-echo "Installing git..."
-sudo apt install -y git
-echo "Completed..."
-echo "Installing ffmpeg..."
-sudo apt install -y ffmpeg
-echo "Completed..."
-echo "Installing curl..."
-sudo apt install -y curl
-echo "Completed..."
 echo "Installing NodeJS..."
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt install -y nodejs
 echo "Completed..."
-echo "Cloning Repository..."
-git clone https://github.com/Kenzuya/Setsu-MD.git
-echo "Completed..."
-echo "Installing Dependencies..."
-cd Setsu-MD
-npm install
-npm install -g pm2
-npm install -g ts-node
-echo "Completed..."
-echo "Starting Bot..."
-npm start
+npx --yes create-setsu-bot
